600894c446eb634b60e62089df28f6508f3d9cfe
CookieBlossom <mariajesuss2424@gmail.com>
2024-08-29 02:46:03 -0400

    home finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -51,7 +51,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(
+      routes, { preloadingStrategy: PreloadAllModules }
+      
+    )
   ],
   exports: [RouterModule]
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { TabsModule } from '../components/tabs/tabs.module';
+import { RouterLinkWithHref } from '@angular/router';
 import { HomePageRoutingModule } from './home-routing.module';
-
-
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    TabsModule,
+    RouterLinkWithHref,
   ],
   declarations: [HomePage]
 })
