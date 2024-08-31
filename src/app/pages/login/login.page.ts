58b2bee81e6000397cd93d8edb937fffdd216bae
Fabian_Iturra <fab.iturra@duocuc.cl>
2024-08-31 02:12:51 -0400

    page login + rute home

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router'; // Importa el Router
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private router: Router) { // Inyecta el Router
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*.,]).+$')
+        ]
+      ]
+    });
   }
 
+  ngOnInit() {}
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      // Aquí deberías realizar la lógica de autenticación (llamar a un servicio de autenticación, etc.)
+      console.log('Formulario válido:', this.loginForm.value);
+      
+      // Suponiendo que la autenticación es exitosa:
+      this.router.navigate(['/home']); // Redirige a la página de inicio
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
+
+  get formControls() {
+    return this.loginForm.controls;
+  }
 }
