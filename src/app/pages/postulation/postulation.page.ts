9d416d0b648a7d5ebbaf65adc19179cc8147a4da
CookieBlossom <mariajesuss2424@gmail.com>
2024-09-01 23:07:32 -0400

    postulation

--- src/app/pages/postulation/postulation.page.ts
+++ src/app/pages/postulation/postulation.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-postulation',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./postulation.page.scss'],
 })
 export class PostulationPage implements OnInit {
+  postulationForm: FormGroup;
+  isToastOpen = false;
+  toastMessage: string = '';
+  Productos: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router:Router, private activatedroute:ActivatedRoute) {
+    this.postulationForm = this.fb.group({
+      position: ['', Validators.required],
+      comment: ['', Validators.required],
+    });
+    this.activatedroute.queryParams.subscribe( param => {
+      if(this.router.getCurrentNavigation()?.extras.state){
+        this.Productos = this.router.getCurrentNavigation()?.extras?.state?.['productos'];
+      }
+    });
+   }
+  ngOnInit(){}
+  irPagina( ruta:string ){
+    let navigationextras:NavigationExtras = {
+      state:{
+        productos: this.Productos,
+      }
+    }
+    this.router.navigate([ruta], navigationextras);
+  }
 
-  ngOnInit() {
+  onSubmit() {
+    if (this.postulationForm.valid) {
+      this.showToast('Exitosa postulación');
+    } else {
+      this.showToast('Te faltan campos por rellenar');
+    }
+  }
+
+  showToast(message: string) {
+    this.toastMessage = message;
+    this.isToastOpen = true;
   }
 
 }
