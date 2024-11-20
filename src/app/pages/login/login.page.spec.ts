3e6a6b3f95598b18b5c3ae1b055770d36fc411d1
Fabian_Iturra <fab.iturra@duocuc.cl>
2024-11-19 21:14:58 -0300

    prueba unitarai login

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -3,36 +3,57 @@ import { LoginPage } from './login.page';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 import { ServiceBDService } from 'src/app/services/service-bd.service';
 import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { UserSessionService } from 'src/app/services/user-session.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let serviceBDSpy: jasmine.SpyObj<ServiceBDService>;
 
   beforeEach(async () => {
+    // Crear un mock para ServiceBDService
+    serviceBDSpy = jasmine.createSpyObj('ServiceBDService', [
+      'searchUsers',
+      'loginUser',
+      'presentAlert',
+      'dbReady',
+    ]);
+
+    // Mockear el método dbReady para devolver un observable válido
+    serviceBDSpy.dbReady.and.returnValue(of(true));
+
     await TestBed.configureTestingModule({
       declarations: [LoginPage],
       imports: [
-        IonicModule.forRoot(),    // Inicializa Ionic
-        FormsModule,              // Soporte para formularios
-        ReactiveFormsModule,      // Soporte para formularios reactivos
-        RouterTestingModule,      // Simula el enrutamiento necesario
+        IonicModule.forRoot(),
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
       ],
       providers: [
-        SQLite,                     // Agrega el provider para SQLite
-        ServiceBDService,
-        NativeStorage        // Agrega el ServiceBDService si depende de SQLite
+        { provide: ServiceBDService, useValue: serviceBDSpy },
+        NativeStorage,
+        UserSessionService,
+        Router,
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();      // Aplica detección de cambios inicial
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('validar correctamente el correo del usuario', () => {
+    const control = component.loginForm.get('email');
+    control?.setValue('usuario@example.com');
+    expect(control?.valid).toBeTrue();
   });
 });
